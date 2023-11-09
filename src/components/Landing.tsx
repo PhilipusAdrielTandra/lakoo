d71c40274efaa0f8e3cc6b47c1ba2d1986e73e87
CH1MP5T0N <philipusadriel@gmail.com>
2023-11-09 18:00:30 +0700

    mobile responsiveness!

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,13 +1,13 @@
 function Landing() {
   return (
-    <div className='h-screen mx-auto flex justify-center items-center bg-white flex-col text-lakoo-red'>
+    <div className='overflow-hidden h-screen mx-auto flex justify-center items-center bg-white flex-col text-lakoo-red'>
       <div className="flex items-center">
-        <h1 className='font-hk text-9xl'>lakoo</h1>
-        <p className='font-hk text-xl ml-4'>JOIN THE MOVEMENT <br/><a className='underline' href="https://chat.whatsapp.com/DcaQ1bjLOkY3kz2GrYgPVs">CLICK ME</a></p>
+        <h1 className='font-hk text-6xl md:text-9xl'>lakoo</h1>
+        <p className='font-hk text-base md:text-xl ml-4'>JOIN THE MOVEMENT <br/><a className='underline' href="https://chat.whatsapp.com/DcaQ1bjLOkY3kz2GrYgPVs">CLICK ME</a></p>
       </div>
-      <p>Join the group and message the admins for more information</p>
-      <br/>
-      <p className='font-hk'>STAY TUNED FOR THE OFFICAL LAUNCH</p>
+      <p className="text-center text-xs md:text-base">Join the group and message the admins for more information</p>
+      <br className="hidden md:block"/>
+      <p className='mt-1 font-hk text-sm md:text-base'>STAY TUNED FOR THE OFFICAL LAUNCH</p>
     </div>
   );
 }
