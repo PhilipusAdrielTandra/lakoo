bf47d51413c86700dd26613ab48a04e42a4cbb58
CH1MP5T0N <philipusadriel@gmail.com>
2023-11-11 20:45:13 +0700

    new changes yipee

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,13 +1,44 @@
+import shirt from '../assets/shirt.jpg'
+import shoe from '../assets/shoe.jpg'
+import jeans from '../assets/jeans.png'
 function Landing() {
   return (
-    <div className='overflow-hidden h-screen mx-auto flex justify-center items-center bg-white flex-col text-lakoo-red'>
-      <div className="flex items-center">
-        <h1 className='font-hk text-6xl md:text-9xl'>lakoo</h1>
-        <p className='font-hk text-base md:text-xl ml-4'>JOIN THE MOVEMENT <br/><a className='underline' href="https://chat.whatsapp.com/DcaQ1bjLOkY3kz2GrYgPVs">CLICK ME</a></p>
+    <div className='h-full md:w-3/6 mx-auto my-8'>
+      <h1 className="text-6xl font-hk text-center text-lakoo-red">lakoo</h1>
+      <h2 className="text-3xl md:mr-24 font-medium text-center">What's it all about?</h2>
+      <div className="flex md:mx-64 my-8 mx-9">
+        <div className="w-2/3">
+          <h1 className='font-hk text-2xl text-lakoo-red'>about us</h1>
+          <p className='text-left'>lakoo is a community based social group dedicated to fashion specific thrifting in Indonesia</p>
+        </div>
+        <div className="w-1/3 my-auto">
+          <img className='' src={shirt}></img>
+        </div>
       </div>
-      <p className="text-center text-xs md:text-base">Join the group and message the admins for more information</p>
-      <br className="hidden md:block"/>
-      <p className='mt-1 font-hk text-sm md:text-base'>STAY TUNED FOR THE OFFICAL LAUNCH</p>
+      <div className="flex md:mx-64 my-8 mx-9">
+      <div className="w-1/3 my-auto">
+          <img className='' src={shoe}></img>
+        </div>
+        <div className="w-2/3">
+          <h1 className='font-hk text-2xl text-right text-lakoo-red'>our vision</h1>
+          <p className='text-right'>lakoo wants to not only create a platform, but a community of thrifters who love to purchase and represent fashion in a sustainable way</p>
+        </div>
+      </div>
+      <div className="flex md:mx-64 my-8 mx-9">
+        <div className="w-2/3">
+          <h1 className='font-hk text-2xl text-lakoo-red'>how we work</h1>
+          <p className=''>currently, lakoo operates through a whatsapp community. lakoo acts as a consignment where we take in secondhand fashion goods to brocast and sell to groupchat participants</p>
+        </div>
+        <div className="w-1/3 my-auto">
+          <img className='' src={jeans}></img>
+        </div>
+      </div>
+      <div className='mx-auto text-center'>
+        <h1 className='font-bold my-4 text-lg'>click below</h1>
+        <a href='https://chat.whatsapp.com/DcaQ1bjLOkY3kz2GrYgPVs'>
+        <button className='bg-lakoo-red p-3 font-hk text-white rounded-2xl text-3xl'>join the movement</button>
+        </a>
+      </div>
     </div>
   );
 }
