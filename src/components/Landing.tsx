675c8df70625caffd5bcb5d3affff18e4bb5dd99
CH1MP5T0N <philipusadriel@gmail.com>
2023-12-21 16:02:08 +0700

    im gonna change this

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,26 +1,42 @@
 import shirt from '../assets/images/shirt.jpg';
 import shoe from '../assets/images/shoe.jpg';
 import jeans from '../assets/images/jeans.png';
+import Carousel from './Carousel';
 
 function Landing() {
+  let slides = [
+    "src/assets/images/carousel.png",
+    "src/assets/images/carousel.png",
+    "src/assets/images/carousel.png",
+  ]
   return (
     <div className="mx-56 mt-14">
       <div className="flex justify-between items-center">
-        <div className="float-left flex items-center">
+        {/* Left Column */}
+        <div className="flex items-center">
           <a href="#" className="font-hk font-semibold">MEN</a>
           <a href="#" className="font-hk font-semibold ml-3">WOMEN</a>
           <a href="#" className="font-hk font-semibold ml-3">UNISEX</a>
         </div>
 
+        {/* Middle Column */}
         <div className="text-center flex items-center">
           <h1 className='text-center font-black font-hk text-lakoo-red text-6xl'>lakoo</h1>
         </div>
 
-        <div className="float-right flex items-center">
+        {/* Right Column */}
+        <div className="flex items-center">
           <a href="#" className="font-hk mr-3">Search</a>
           <a href="#" className="font-hk">Cart</a>
         </div>
       </div>
+
+      {/* Carousel in the middle */}
+      <div className="text-center my-12">
+        <Carousel slides={slides}/>
+      </div>
+
+      <h1 className='text-center '>SHOP OUR LATEST DROP</h1>
     </div>
   );
 }
