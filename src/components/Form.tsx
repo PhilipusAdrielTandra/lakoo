373e2d43be94d09315a64313d46848f85a70d1d8
kim <91533432+kimberlymazel@users.noreply.github.com>
2024-01-09 14:10:57 +0700

    changed style to black

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -46,7 +46,7 @@ export default function Example() {
                   <div className="mt-4 flex text-sm leading-6 text-gray-600">
                     <label
                       htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-red-800 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-red-500"
                     >
                       <span>Upload a file</span>
                       <input id="file-upload" name="file-upload" type="file" className="sr-only" />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-red-900">
+    <Disclosure as="nav" className="bg-black">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -33,11 +33,12 @@ export default function Example() {
               </div>
               
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                
                 {/* LOGO */}
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="h-5 w-auto"
-                    src="src/assets/images/logotext.png"
+                    src="src/assets/images/logotextred.png"
                     alt="Lakoo"
                   />
                 </div>
@@ -50,7 +51,7 @@ export default function Example() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-red-700 hover:text-white',
+                          item.current ? 'bg-red-900 text-white' : 'text-gray-300 hover:bg-gray-900 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -66,7 +67,7 @@ export default function Example() {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
-                  className="relative rounded-full bg-red-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-red-800"
+                  className="relative rounded-full bg-black p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-1 focus:ring-white"
                 >
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">View notifications</span>
