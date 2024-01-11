9abf7dce37b55bceab4686ac1efcf40bf3acee04
kim <91533432+kimberlymazel@users.noreply.github.com>
2024-01-11 13:57:18 +0700

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -354,7 +354,7 @@ const ProductForm = () => {
           <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
               <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">IDR</span>
               <input
-                type="text"
+                type="number"
                 name="price"
                 id="item-price"
                 autoComplete="item-price"
